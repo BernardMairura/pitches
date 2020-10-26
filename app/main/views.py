80efa9e4930981897821aabb3350a9da14729e55
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-26 17:30:47 +0300

    add bootstrap

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+
+@app.route('/pitch/<int:pitch_id>')
+def movie(pitch_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('pitch.html',id = pitch_id)
