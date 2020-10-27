a91b291e18bf8e447b241fa4e638522979f6ad2c
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-27 17:55:42 +0300

    re-generating alembic file after errors on migration

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
     pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
@@ -40,15 +40,6 @@ class User(db.Model):
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
     
-
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
 
 
 
