03e9662caea537c96f677637d7fc265cf180f91e
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-28 13:18:41 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bernard:86kerubo19@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
   
