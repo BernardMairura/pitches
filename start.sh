5f2ce48948bd451edeed99c341b25c2a2eea4d01
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-10-28 20:42:51 +0300

    start.sh not ignored

---
+++ start.sh
@@ -0,0 +1,8 @@
+export SECRET_KEY='mairura19'
+export DATABASE_URL='postgres://vhndqvsflrlzvy:213af3e8f288ea02afa21028260d0b7d4a75bad4cb0099448f2960709602ae46@ec2-23-20-70-32.compute-1.amazonaws.com:5432/d75u2bismj7q0o'
+
+
+
+
+
+python3.9 manage.py server
